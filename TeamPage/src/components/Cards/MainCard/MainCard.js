b9f1f955bbf704b8b1cfa7a956b98ae19ce5c582
cn-eveer <sienreeve46@gmail.com>
2021-12-22 13:59:16 +0900

    add props to MainCard

--- TeamPage/src/components/Cards/MainCard/MainCard.js
+++ TeamPage/src/components/Cards/MainCard/MainCard.js
@@ -9,13 +9,15 @@ import LogoRight from './svg/logo-right.svg'
 import React from 'react'
 import Row from 'react-bootstrap/Row'
 
-export const MainCard = () => {
+
+export const MainCard = ({ props }) => {
+
     return (
         <div>
             <Card className="text-white">
                 <Card.Img src={ Image } className="main-img"/>
                 <Card.ImgOverlay className='overlay-component'>
-                    <Card.Title className='card-title'>GDSC Waseda</Card.Title>
+                    <Card.Title className='card-title'>{ props.title }</Card.Title>
                     <Container>
                         <Row className="justify-content-md-center">
                             <Col md='auto'>
@@ -23,7 +25,7 @@ export const MainCard = () => {
                             </Col>
                             <Col md='auto'>
                                 <Card.Text className='team-name'>
-                                    Frontend Team
+                                    { props.teamName }
                                 </Card.Text>
                             </Col>
                             <Col md='auto'>
@@ -32,9 +34,7 @@ export const MainCard = () => {
                         </Row>
                     </Container>
                     <p>
-                        "We must hold our organizations accountable where they control <br/>
-                        the doors by demanding clarity and tranparency around <br/>
-                        succession planning." - Ruth Porat, CFO at Alptha
+                        { props.description }
                     </p>
                 </Card.ImgOverlay>
             </Card>
